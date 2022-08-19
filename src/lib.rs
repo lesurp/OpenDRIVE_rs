6729daceb3b8a97149f2b9e5414a9fcca7bdf1bd
Paul Lesur <git@lesurpaul.fr>
2022-08-19 14:55:02 +0200

    "fix" import (make it compile, but I don'get why)

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,9 @@
 mod automagic;
 mod open_drive;
 
-pub use automagic::open_drive::*;
-pub use automagic::*;
+// TODO: Why do I need to export this manually?
+// The wildcard appears to only export child modules...
+pub use automagic::{open_drive::RoadType, open_drive::*, OpenDrive};
 pub use open_drive::OpenDriveError;
 
 #[cfg(test)]
@@ -14,5 +15,11 @@ mod tests {
         let mut dir = std::path::PathBuf::from(env!("CARGO_MANIFEST_DIR"));
         dir.push("test/Town01.xodr");
         let _ = OpenDrive::from_path(dir).unwrap();
+    }
+
+    #[test]
+    fn test_imports() {
+        use crate::RoadType;
+        let r = RoadType::default();
     }
 }
